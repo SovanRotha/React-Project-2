94324d0254bd0a27474350cc4671d0f9f5c1a2b2
sovanrotha.pit <sovanrothapit@gmail.com>
2026-01-10 15:17:21 +0700

    commit to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import Header from "./components/Header";
 import Home from "./project/Home";
 import About from "./project/About";
+import {Routes, Route} from "react-router-dom";
 import Explore from "./project/Explore";
 import MovieDetail from "./project/DetailMovies";
 import Scroll from "./components/scroll";
-import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="bg-gray-800 dark:bg-white">
-      <Scroll />
-      <Header /> {/* Header outside Routes, always visible */}
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Explore" element={<Explore />} />
-        <Route path="/MovieDetail/:id" element={<MovieDetail />} />
-      </Routes>
-    </div>
+function App()
+{
+  return(
+    <>
+      <div className="bg-gray-800 dark:bg-white">
+        <Scroll/>
+         <Header/>
+         <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/About" element={<About/>}></Route>
+            <Route path="/Explore" element={<Explore/>}></Route>
+            <Route path="/MovieDetail/:id" element={<MovieDetail/>}></Route>
+         </Routes>
+      </div>
+    </>
+   
   );
 }
-
 export default App;
